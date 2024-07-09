cc857a7e425fe2a7f85a484c93733cb872376d4a
Prasad48432 <prasadreddymar6@gmail.com>
2024-07-09 18:43:16 +0530

    initial commit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
+
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
@@ -15,12 +16,14 @@ def transform_text(text):
     for i in text:
         if i.isalnum():
             y.append(i)
+
     text = y[:]
     y.clear()
 
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
+
     text = y[:]
     y.clear()
 
@@ -34,27 +37,18 @@ model = pickle.load(open('model.pkl','rb'))
 
 st.title("Email/SMS Spam Classifier")
 
-input_sms =  st.text_input("Enter the Message")
+input_sms = st.text_area("Enter the message")
 
-# Preprocess
+if st.button('Predict'):
 
-transformed_sms = transform_text(input_sms)
-
-
-# Vectorize
-
-
-vector_input = tfidf.transform([transformed_sms])
-
-
-# Predict
-
-result = model.predict(vector_input)[0]
-
-
-# Display
-
-if result == 1:
-    st.header("Spam")
-else:
-    st.header("Not Spam")
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
